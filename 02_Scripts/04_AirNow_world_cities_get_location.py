9534f3189cf1c7fb167194a5465a38d560fab5aa
avaimar <andreavalle.ai@gmail.com>
2021-01-24 18:06:08 -0600

    Added script 06 to combine data from both AirNow datasets. Generated final output dataset World_locations_2020_avg.csv

--- 02_Scripts/04_AirNow_world_cities_get_location.py
+++ 02_Scripts/04_AirNow_world_cities_get_location.py
@@ -12,10 +12,11 @@ cities_data = pd.read_csv(
 # Get unique city names
 cities = cities_data['name'].unique()
 
-# Get lat lon
+# Get lat and lon
 base_url = 'https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}'
 locations = []
-for city in cities:
+for i, city in enumerate(cities):
+    print("[INFO] Requesting data for city {}/{}".format(i, len(cities)))
     # Transform city string and request geolocation
     city_str = city.replace(' ', '%20')
     try:
@@ -26,10 +27,11 @@ for city in cities:
     # Extract latitude and longitude
     r = r.json()
     if r['status'] == 'OK':
-        lat = r['results'][0]['geometry']['location']['lat']
-        lon = r['results'][0]['geometry']['location']['lng']
-        locations.append((city, lat, lon))
-        if len(r['results']) > 1:
+        if len(r['results']) == 1:
+            lat = r['results'][0]['geometry']['location']['lat']
+            lon = r['results'][0]['geometry']['location']['lng']
+            locations.append((city, lat, lon))
+        else:
             print('[WARNING] Multiple matches for {}'.format(city))
     else:
         print("[INFO] Error obtaining location for {}".format(city))
@@ -40,5 +42,6 @@ city_table.columns = ['name', 'lat', 'lon']
 cities_data = pd.merge(cities_data, city_table, how='left',
                        on='name')
 
-# Manually get locations with multiple matches
-
+# Save data
+cities_data.to_csv(
+    '../01_Data/01_Carbon_emissions/AirNow/World_cities_2020_avg_latlon.csv')
