Fixed tensor size bug, added mean and SD computation for normalization

Models/data_loaders.py
@@ -7,22 +7,47 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
 import build_dataset
+import utils
 
 
 # Define data transforms
-# TODO Seems like we do need to normalize inputs --- should we compute and use the mean for each of the remaining bands?
-data_transforms = {
-    'train': transforms.Compose([
-        # mean-subtraction normalization https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'test': transforms.Compose([
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-}
+def define_data_transforms(training_band_means, training_band_sds):
+    """
+    Define the transforms to be applied to the data.
+    :param training_band_sds: (dict) the sds for each band computed from the training
+        set
+    :param training_band_means: (dict) the means for each band computed from the training
+        set
+    :return: (dict) of transforms for each type of dataset
+    """
+    # ImageNet means and sds
+    # https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
+    image_net_means = [0.485, 0.456, 0.406]
+    image_net_sds = [0.229, 0.224, 0.225]
+
+    # Get means and sds for our training set
+    training_band_means = [training_band_means['band_{}'.format(i)] for i in range(7)]
+    training_band_sds = [training_band_sds['band_{}'.format(i)] for i in range(7)]
+
+    # Use means and sds for channels 4 to 7 from our training set
+    image_net_means.extend(training_band_means[3:])
+    image_net_sds.extend(training_band_sds[3:])
+
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(tuple(image_net_means), tuple(image_net_sds))
+        ]),
+        'val': transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(tuple(image_net_means), tuple(image_net_sds))
+        ]),
+        'test': transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(tuple(image_net_means), tuple(image_net_sds))
+        ])
+    }
+    return data_transforms
 
 
 # Satellite Data class for dataloaders
@@ -40,7 +65,7 @@ class SatelliteData(Dataset):
         :param transform (torchvision.transforms)
         """
         # Load files in path
-        data_path = os.path.join(data_dir, '{}_{}/*'.format(output_variable, split))
+        data_path = os.path.join(data_dir, output_variable, '{}/*'.format(split))
         data_points = glob.glob(data_path)
         self.data_points = data_points
         self.m = len(data_points)
@@ -69,11 +94,14 @@ class SatelliteData(Dataset):
 
         # Get features and labels
         X_item = data['X']
-        Y_item = data['y'].reshape(1, 1)
+        Y_item = data['y']
 
         # Apply transforms
         if self.transform:
-            X_item, Y_item = self.transform(X_item), self.transform(Y_item)
+            # We must reorder the dimensions as torch assumes numpy images
+            # are in the format (H,W,C)
+            X_item = np.transpose(X_item, (1, 2, 0))
+            X_item = self.transform(X_item)
         return X_item, Y_item
 
 
@@ -94,21 +122,30 @@ def fetch_dataloader(dataset_types, data_dir, output_variable, params,
     """
 
     # Build datasets for selected output variable if they do not exist
-    file_path = os.path.join(data_dir, '{}_{}'.format(output_variable, 'train'))
+    file_path = os.path.join(data_dir, output_variable)
     if not os.path.exists(file_path):
         print('[INFO] Building dataset...')
+        os.mkdir(file_path)
         build_dataset.process_sat_data(
             base_data_file, data_dir, output_variable, data_split)
 
     # Use GPU if available
     use_cuda = torch.cuda.is_available()
 
+    # Get mean and sd dictionaries from our training set and define transforms
+    training_band_means = utils.load_dict(
+        os.path.join(data_dir, output_variable, 'band_means.json'))
+    training_band_sds = utils.load_dict(
+        os.path.join(data_dir, output_variable, 'band_sds.json'))
+    transforms_dict = define_data_transforms(training_band_means, training_band_sds)
+
     # Get data loaders
     dataloaders = {}
 
     for split in ['train', 'val', 'test']:
         if split in dataset_types:
-            data = SatelliteData(data_dir, output_variable, split)
+            data = SatelliteData(
+                data_dir, output_variable, split, transform=transforms_dict[split])
             dl = DataLoader(
                 dataset=data, batch_size=params['batch_size'], shuffle=True,
                 num_workers=params['num_workers'], pin_memory=use_cuda)

train.py
@@ -42,7 +42,6 @@ def train(model, optimizer, loss_fn, dataloader, metrics, params):
     # Set summary lists
     metrics_summary = []
     losses = []
-    loss_avg = 0
 
     for i, (train_batch, labels_batch) in enumerate(dataloader):
         # Check for GPU and send variables
@@ -56,7 +55,7 @@ def train(model, optimizer, loss_fn, dataloader, metrics, params):
 
         # Forward propagation, loss computation and backpropagation
         output_batch = model(train_batch)
-        loss = loss_fn(output_batch, labels_batch) # TODO getting error: Using a target size (torch.Size([1, 1, 1])) that is different to the input size when computing MSE Loss
+        loss = loss_fn(output_batch, labels_batch)
         optimizer.zero_grad()
         loss.backward()
 
@@ -74,10 +73,18 @@ def train(model, optimizer, loss_fn, dataloader, metrics, params):
             # Compute metrics
             summary_batch = {metric: metrics[metric](output_batch, labels_batch)
                              for metric in metrics}
-            metrics_summary.append(summary_batch)
             summary_batch['loss'] = loss.item()
+            metrics_summary.append(summary_batch)
+
+            # Append loss and compute average loss
             losses.append(loss.item())
-            loss_avg = np.mean(losses)
+            print("Running average training loss: {:2f}".format(np.mean(losses)))
+            logger.write("Running average training loss: {:2f}".format(np.mean(losses)))
+
+    # Compute metrics mean and add to logger
+    metrics_mean = {metric: np.mean([x[metric] for x in metrics_summary]) for metric in metrics}
+    logger.write(metrics_mean)
+    return losses
 
 
 def train_and_evaluate(model, optimizer, loss_fn, train_dataloader,
@@ -112,7 +119,7 @@ def train_and_evaluate(model, optimizer, loss_fn, train_dataloader,
         # Train single epoch on the training set
         print('[INFO] Training Epoch {}/{}'.format(
             epoch + 1, params['num_epochs']))
-        train(model, optimizer, loss_fn, train_dataloader, metrics, params)
+        train_losses = train(model, optimizer, loss_fn, train_dataloader, metrics, params)
 
         # Evaluate single epoch on the validation set
         val_metrics = evaluate(model, loss_fn, val_dataloader, metrics, params)